dea157ca8ae2105cd79d0413828f02508ac2eee7
matt venn <matt@mattvenn.net>
2017-11-20 16:09:59 +0100

    testing right and left sides

--- Raise-Firmware.ino
+++ Raise-Firmware.ino
@@ -24,6 +24,8 @@
 #include "Kaleidoscope-LED-Stalker.h"
 #include "Kaleidoscope-LED-AlphaSquare.h"
 #include "Kaleidoscope-Model01-TestMode.h"
+#include "Kaleidoscope-LEDEffect-DigitalRain.h"
+#include "Kaleidoscope-Underglow-LEDEffect-Chase.h"
 
 #define MACRO_VERSION_INFO 1
 #define Macro_VersionInfo M(MACRO_VERSION_INFO)
@@ -36,30 +38,31 @@
   Key_Escape, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6,             Key_7, Key_8, Key_9, Key_0, Key_A, Key_A, Key_Delete, \
   Key_Tab, Key_Q, Key_UpArrow, Key_E, Key_R, Key_T,                       Key_Y, Key_U, Key_I, Key_O, Key_P, Key_M, Key_Backspace, Key_1, \
   Key_LeftControl, Key_LeftArrow, Key_DownArrow, Key_RightArrow, Key_F, Key_G,               Key_H, Key_J, Key_K, Key_L, Key_L, Key_M, Key_Enter, \
-  Key_LeftShift, Key_B, Key_Z, Key_X, Key_C, Key_V, Key_B,          Key_N, Key_M, Key_Semicolon, Key_Comma, Key_Minus, Key_RightShift, \
+  Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B,          Key_N, Key_M, Key_Semicolon, Key_Comma, Key_Minus, Key_RightShift, \
   Key_LeftControl, Key_CapsLock, ___, Key_Keymap1, Key_Spacebar,    Key_Spacebar, Key_RightAlt, Key_Keymap1, ___, Key_RightControl, Key_1,  \
                        Key_Backspace, Key_Enter,            Key_Backspace, Key_Keymap1)
 
 #define QWERTY KEYMAP_60 ( \
-Key_Escape, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6,                     Key_7, Key_8, Key_9, Key_0, Key_Minus, Key_Equals, Key_Delete, \
+Key_Escape, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6,                     Key_LEDEffectNext, Key_8, Key_9, Key_0, Key_Minus, Key_Equals, Key_Delete, \
 Key_Tab, Key_Q, Key_W, Key_E, Key_R, Key_T,                               Key_Y, Key_U, Key_I, Key_O, Key_P, Key_LeftBracket, Key_Backspace, Key_1, \
-Key_CapsLock, Key_A, Key_S, Key_D, Key_F, Key_G,                           Key_H, Key_J, Key_K, Key_L, Key_Semicolon, Key_Quote, Key_Enter, \
-Key_LeftShift, Key_Period, Key_Z, Key_X, Key_C, Key_V, Key_B,             Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_RightShift,\
+Key_CapsLock, Key_LEDEffectNext , Key_S, Key_D, Key_F, Key_G,                           Key_H, Key_J, Key_K, Key_L, Key_Semicolon, Key_Quote, Key_Enter, \
+Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B,             Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_RightShift,\
 Key_LeftControl,Key_LeftGui, Key_LeftAlt,Key_Keymap1, Key_Spacebar,  Key_Spacebar, Key_RightAlt, Key_RightGui, Key_Menu, Key_RightControl, Key_1,\
-                     Key_LEDEffectNext, Key_Enter,                  Key_Backspace, Key_Keymap1)
+                     Key_LEDEffectNext, Key_Enter,                  Key_A, Key_B)
 
 const Key keymaps[][ROWS][COLS] PROGMEM = {
   QWERTY,
   NUMPAD
 };
 
-static kaleidoscope::LEDSolidColor solidRed(160, 0, 0);
+static kaleidoscope::LEDSolidColor solidRed(255, 0, 0);
 static kaleidoscope::LEDSolidColor solidOrange(140, 70, 0);
 static kaleidoscope::LEDSolidColor solidYellow(130, 100, 0);
-static kaleidoscope::LEDSolidColor solidGreen(0, 160, 0);
-static kaleidoscope::LEDSolidColor solidBlue(0, 70, 130);
+static kaleidoscope::LEDSolidColor solidGreen(0, 255, 0);
+static kaleidoscope::LEDSolidColor solidBlue(0, 0, 255);
 static kaleidoscope::LEDSolidColor solidIndigo(0, 0, 170);
 static kaleidoscope::LEDSolidColor solidViolet(130, 0, 120);
+static kaleidoscope::LEDSolidColor solidWhite(255, 255, 255);
 
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   if (macroIndex == TOGGLENUMLOCK && keyToggledOn(keyState)) {
@@ -82,25 +85,30 @@ void setup() {
   Kaleidoscope.setup(KEYMAP_SIZE);
   BootKeyboard.begin();
   Kaleidoscope.use(
-    &BootGreetingEffect,
-    &TestMode,
-    &LEDControl, &LEDOff,
-    &LEDRainbowEffect, &LEDRainbowWaveEffect, &LEDChaseEffect,
-    &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidIndigo, &solidViolet,
+//    &BootGreetingEffect,
+//    &TestMode,
+    &LEDControl,
+    //&LEDOff,
+    &LEDRainbowEffect, &LEDRainbowWaveEffect, 
+    &LEDChaseEffect,
+    &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidIndigo, &solidViolet, &solidWhite,
     &LEDBreatheEffect,
-    &AlphaSquareEffect,
+    //&AlphaSquareEffect,
     &StalkerEffect,
+    &LEDDigitalRainEffect,
+    //&LEDUnderglowChaseEffect,
     &NumLock,
     &Macros,
     &MouseKeys,
     NULL);
 
   NumLock.numPadLayer = NUMPAD_KEYMAP;
-  AlphaSquare.color = { 255, 0, 0 };
-  LEDRainbowEffect.brightness(150);
-  LEDRainbowWaveEffect.brightness(150);
+//  AlphaSquare.color = { 255, 0, 0 };
+//  LEDRainbowEffect.brightness(150);
+//  LEDRainbowWaveEffect.brightness(150);
   StalkerEffect.variant = STALKER(BlazingTrail);
-  LEDOff.activate();
+//  StalkerEffect.activate();
+    StalkerEffect.activate();
 }
 
 
