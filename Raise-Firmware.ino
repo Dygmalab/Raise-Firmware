01c6f045e19e0ea468ddf0a5b797013073634e5c
matt venn <matt@mattvenn.net>
2018-09-21 13:49:39 +0200

    working on joint effect

--- Raise-Firmware.ino
+++ Raise-Firmware.ino
@@ -47,6 +47,7 @@
 
 // Support for LED modes that pulse the keyboard's LED in a rainbow pattern
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
+#include "Kaleidoscope-LEDEffect-Joint.h"
 
 // Support for an LED mode that lights up the keys as you press them
 #include "Kaleidoscope-LED-Stalker.h"
@@ -136,7 +137,7 @@ enum { QWERTY, NUMPAD, _LAYER_MAX }; // layers
 
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 [QWERTY] = KEYMAP_60 
-( Key_Escape, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6,                     Key_LEDEffectNext, Key_8, Key_9, Key_0, Key_Minus, Key_Equals, Key_Delete, 
+( Key_Escape, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6,                     Key_LEDEffectNext, Key_LEDEffectNext, Key_9, Key_0, Key_Minus, Key_Equals, Key_Delete, 
   Key_Tab, Key_Q, Key_W, Key_E, Key_R, Key_T,                               Key_Y, Key_U, Key_I, Key_O, Key_P, Key_LeftBracket, Key_Backspace, Key_1, 
   Key_CapsLock, Key_LEDEffectNext , Key_S, Key_D, Key_F, Key_G,                           Key_H, Key_J, Key_K, Key_L, Key_Semicolon, Key_Quote, Key_Enter, 
   Key_LeftShift, ___, Key_Z, Key_X, Key_C, Key_V, Key_B,                    Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_RightShift,
@@ -243,6 +244,7 @@ KALEIDOSCOPE_INIT_PLUGINS(
     LEDControl,
     LEDOff,
     LEDRainbowEffect,
+    LEDJointEffect,
     LEDRainbowWaveEffect,
     LEDChaseEffect,
     solidRed, solidGreen, solidBlue, solidWhite,
@@ -308,8 +310,9 @@ void setup() {
   // We want to make sure that the firmware starts with LED effects off
   // This avoids over-taxing devices that don't have a lot of power to share
   // with USB devices
+  LEDJointEffect.activate();
   //LEDRainbowWaveEffect.activate();
-  solidGreen.activate();
+  //LEDBreatheEffect.activate();
   //LEDOff.activate();
   //StalkerEffect.activate();
 }
