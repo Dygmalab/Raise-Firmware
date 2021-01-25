81b0f3bbfd693708b00e69a4f80b035433894d51
alexpargon <alexpargon@gmail.com>
2021-01-25 09:39:30 +0100

    Adding the Watchdog timer configured for 1 second restarts
    Signed-off-by: alexpargon <alexpargon@gmail.com>

--- Raise-Firmware.ino
+++ Raise-Firmware.ino
@@ -183,6 +183,29 @@ KALEIDOSCOPE_INIT_PLUGINS(
 );
 
 void setup() {
+ // Set up the generic clock (GCLK2) used to clock the watchdog timer at 1.024kHz
+  REG_GCLK_GENDIV = GCLK_GENDIV_DIV(4) |            // Divide the 32.768kHz clock source by divisor 32, where 2^(4 + 1): 32.768kHz/32=1.024kHz
+                    GCLK_GENDIV_ID(2);              // Select Generic Clock (GCLK) 2
+  while (GCLK->STATUS.bit.SYNCBUSY);                // Wait for synchronization
+
+  REG_GCLK_GENCTRL = GCLK_GENCTRL_DIVSEL |          // Set to divide by 2^(GCLK_GENDIV_DIV(4) + 1)
+                     GCLK_GENCTRL_IDC |             // Set the duty cycle to 50/50 HIGH/LOW
+                     GCLK_GENCTRL_GENEN |           // Enable GCLK2
+                     GCLK_GENCTRL_SRC_OSCULP32K |   // Set the clock source to the ultra low power oscillator (OSCULP32K)
+                     GCLK_GENCTRL_ID(2);            // Select GCLK2         
+  while (GCLK->STATUS.bit.SYNCBUSY);                // Wait for synchronization
+
+  // Feed GCLK2 to WDT (Watchdog Timer)
+  REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |           // Enable GCLK2 to the WDT
+                     GCLK_CLKCTRL_GEN_GCLK2 |       // Select GCLK2
+                     GCLK_CLKCTRL_ID_WDT;           // Feed the GCLK2 to the WDT
+  while (GCLK->STATUS.bit.SYNCBUSY);                // Wait for synchronization
+
+  REG_WDT_CONFIG = WDT_CONFIG_PER_512;              // Set the WDT reset timeout to 2 second
+  while(WDT->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+  REG_WDT_CTRL = WDT_CTRL_ENABLE;                   // Enable the WDT in normal mode
+  while(WDT->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+
   Kaleidoscope.serialPort().begin(9600);
   Kaleidoscope.setup();
 
@@ -210,6 +233,11 @@ void setup() {
 }
 
 void loop() {
+  if (!WDT->STATUS.bit.SYNCBUSY)                // Check if the WDT registers are synchronized
+  {
+    REG_WDT_CLEAR = WDT_CLEAR_CLEAR_KEY;        // Clear the watchdog timer
+  }
+  // Application code goes here...
   Kaleidoscope.loop();
   protocolBreathe();
 }
